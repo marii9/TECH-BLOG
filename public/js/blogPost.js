878324cb768bcd42c4cee5181320039f17c4d568
marii9 <116024194+marii9@users.noreply.github.com>
2023-03-18 23:50:10 -0400

    Update blogPost.js

--- public/js/blogPost.js
+++ public/js/blogPost.js
@@ -1,47 +1,89 @@
-const newFormHandler = async (event) => {
+const newPostForm = document.querySelector('#new-post-form');
+
+newPostForm.addEventListener('submit', async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#project-name').value.trim();
-  const needed_funding = document.querySelector('#project-funding').value.trim();
-  const description = document.querySelector('#project-desc').value.trim();
+  const title = document.querySelector('#post-title').value;
+  const content = document.querySelector('#post-content').value;
 
-  if (name && needed_funding && description) {
-    const response = await fetch(`/api/projects`, {
+  try {
+    const response = await fetch('/api/blog-posts', {
       method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       },
+      body: JSON.stringify({
+        title,
+        content
+      })
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      alert('Post created successfully!');
+      window.location.href = '/dashboard';
     } else {
-      alert('Failed to create project');
+      throw new Error('Failed to create post');
     }
+  } catch (error) {
+    console.error(error);
+    alert('Failed to create post');
   }
-};
+});
 
-const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/projects/${id}`, {
-      method: 'DELETE',
-    });
 
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert('Failed to delete project');
-    }
+
+
+
+const updatePostForm = document.getElementById('update-post-form');
+
+updatePostForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const titleInput = document.getElementById('#post-title');
+  const contentInput = document.getElementById('#post-content');
+
+  const title = titleInput.value;
+  const content = contentInput.value;
+
+  const postId = updatePostForm.dataset.postId;
+
+  const response = await fetch(`/api/blog-posts/update-posts`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ title, content })
+  });
+
+  if (response.ok) {
+    // Redirect to the dashboard after updating the post
+    window.location.href = '/dashboard';
+  } else {
+    // Handle the error case
+    const error = await response.json();
+    console.error(error.message);
   }
-};
+});
 
-document
-  .querySelector('.new-project-form')
-  .addEventListener('submit', newFormHandler);
+const deletePostForm = document.getElementById('delete-post-form');
 
-document
-  .querySelector('.project-list')
-  .addEventListener('click', delButtonHandler);
+deletePostForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const postId = deletePostForm.dataset.postId;
+
+  const response = await fetch(`/api/blog-posts/delete-post`, {
+    method: 'DELETE',
+    body: JSON.stringify({ postId })
+  });
+
+  if (response.ok) {
+    // Reload the page to show the updated list of posts
+    window.location.reload();
+  } else {
+    // Handle the error case
+    const error = await response.json();
+    console.error(error.message);
+  }
+});
