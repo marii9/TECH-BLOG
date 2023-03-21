75c2ca042a552c5d8a24f947da59e5610dfe6c25
marii9 <116024194+marii9@users.noreply.github.com>
2023-03-20 22:17:25 -0400

    Update blogPost.js

--- public/js/blogPost.js
+++ public/js/blogPost.js
@@ -35,37 +35,36 @@ newPostForm.addEventListener('submit', async (event) => {
 
 
 
-const updatePostForm = document.getElementById('update-post-form');
+// const updatePostForm = document.getElementById('update-post-form');
 
-updatePostForm.addEventListener('submit', async (event) => {
-  event.preventDefault();
+// updatePostForm.addEventListener('submit', async (event) => {
+//   event.preventDefault();
 
-  const titleInput = document.getElementById('post-title');
-  const contentInput = document.getElementById('post-content');
+//   const titleInput = document.getElementById('post-title');
+//   const contentInput = document.getElementById('post-content');
 
-  const title = titleInput.value;
-  const content = contentInput.value;
+//   const title = titleInput.value;
+//   const content = contentInput.value;
 
-  const postId = updatePostForm.dataset.postId;
+//   const postId = updatePostForm.dataset.postId;
 
-  const response = await fetch(`/api/blog-posts/update-posts/${postId}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ title, content })
-  });
+//   const response = await fetch(`/api/blog-posts/update-posts/${postId}`, {
+//     method: 'PUT',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({ title, content })
+//   });
 
-  if (response.ok) {
-    // Redirect to the dashboard after updating the post
-    window.location.href = '/dashboard';
-  } else {
-    // Handle the error case
-    const error = await response.json();
-    console.error(error.message);
-  }
-});
-
+//   if (response.ok) {
+//     // Redirect to the dashboard after updating the post
+//     window.location.href = '/dashboard';
+//   } else {
+//     // Handle the error case
+//     const error = await response.json();
+//     console.error(error.message);
+//   }
+// });
 
 const deletePostForm = document.getElementById('delete-post-form');
 
@@ -74,14 +73,14 @@ deletePostForm.addEventListener('submit', async (event) => {
 
   const postId = deletePostForm.dataset.postId;
 
-  const response = await fetch(`/api/blog-posts/delete-post`, {
+  const response = await fetch(`/api/blog-posts/delete-post/${postId} `, {
     method: 'DELETE',
-    body: JSON.stringify({ postId })
+    body: JSON.stringify({title, content })
   });
 
   if (response.ok) {
     // Reload the page to show the updated list of posts
-    window.location.reload();
+    window.location.href = '/dashboard';
   } else {
     // Handle the error case
     const error = await response.json();
