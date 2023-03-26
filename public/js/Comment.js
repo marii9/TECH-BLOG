7771abab46fc810875fed5ce67fb50ca7cf015dd
marii9 <116024194+marii9@users.noreply.github.com>
2023-03-26 00:22:27 -0400

    Update Comment.js

--- public/js/Comment.js
+++ public/js/Comment.js
@@ -32,12 +32,49 @@ console.log(content);
   }
 });
 
+const commentForms = document.querySelectorAll('.new-comment-form');
+
+commentForms.forEach((form) => {
+  form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const title = form.getAttribute("data-post-id");
+    const name = form.querySelector('#new-name').value;
+    const content = form.querySelector('#comment-content').value;
+    
+    try {
+      const response = await fetch(`/api/blog-posts/comments/${title}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          title,
+          name,
+          content
+        })
+      });
+
+      if (response.ok) {
+        alert('Comment created successfully!');
+        window.location.reload();
+      } else {
+        throw new Error('Failed to create comment');
+      }
+    } catch (error) {
+      console.error(error);
+      alert('Failed to create comment');
+    }
+  });
+});
+
 const updateCommentForm = document.getElementById('update-comment-form');
+console.log(updateCommentForm);
 
 updateCommentForm.addEventListener('submit', async (event) => {
   event.preventDefault();
 
-  const postId = updateCommentForm.dataset.postId;
+  const postId =  updateCommentForm.getAttribute('data-post-id');
+  console.log(postId);
   const content = document.querySelector('#comment-content').value;
 
   const response = await fetch(`/api/blog-posts/update-comment/${postId}`, {
